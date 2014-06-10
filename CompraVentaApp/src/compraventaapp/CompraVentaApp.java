33180d81cd5fc0e59d293e0f3704ac5f196bc6cb
ldrc2895 <ldrc2895@gmail.com>
2014-06-11 01:55:16 +0200

    Creacion proyecto

---
+++ CompraVentaApp/src/compraventaapp/CompraVentaApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compraventaapp;
+
+/**
+ *
+ * @author LuisDiego
+ */
+public class CompraVentaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
